6a4b51081523f8a84730387c23749daf37583215
laktossa <karman.sony@gmail.com>
2023-03-05 20:49:23 +0700

    Init Project

---
+++ backend/src/test/java/cerita/ke/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cerita.ke.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
